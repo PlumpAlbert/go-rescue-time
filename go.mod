f39d966137ca72bfe4f6ffc98b9f5d82ad8a9ebf
plump_albert <plumpalbert@gmail.com>
2023-02-12 00:34:02 +0300

    added color output

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/plumpalbert/go-rt
 
 go 1.20
+
+require github.com/TwiN/go-color v1.4.0 // indirect
--- main.go
+++ main.go
@@ -7,26 +7,54 @@ import (
 	"net/http"
 	"net/url"
 	"time"
+
+	"github.com/TwiN/go-color"
 )
 
 var ENDPOINT = "https://www.rescuetime.com/anapi/data"
 var COLUMNS = [5]int{VeryProductive, Productive, Neutral, Distracting, VeryDistracting}
 
-func center(w int, s string) {
-	fmt.Printf("%*s", -w, fmt.Sprintf("%*s", (w+len(s))/2, s))
+func center(w int, s string) string {
+	return fmt.Sprintf("%*s", -w, fmt.Sprintf("%*s", (w+len(s))/2, s))
+}
+
+func printHead() {
+	fmt.Printf("%*s", -9, fmt.Sprintf("%*s", 5, "📅"))
+	fmt.Printf("%*s", -9, fmt.Sprintf("%*s", 5, "🥰"))
+	fmt.Printf("%*s", -9, fmt.Sprintf("%*s", 5, "😊"))
+	fmt.Printf("%*s", -9, fmt.Sprintf("%*s", 5, "😐"))
+	fmt.Printf("%*s", -9, fmt.Sprintf("%*s", 5, "😟"))
+	fmt.Printf("%*s", -9, fmt.Sprintf("%*s", 5, "😢"))
+	fmt.Println()
 }
 
 func printRow(key time.Time, data Productivity) {
-	center(10, key.Format("2006-01-02"))
+	fmt.Print(color.Ize(color.Green, center(10, key.Format("2006-01-02"))))
 	for _, i := range COLUMNS {
+		var s string
 		item := data[i]
 		if item == 0 {
-			center(10, "00:00")
-			continue
+			s = center(10, "00:00")
+		} else {
+			hours := data[i] / 3600
+			minutes := (data[i] % 3600) / 60
+			s = center(10, fmt.Sprintf("%02d:%02d", hours, minutes))
 		}
-		hours := data[i] / 3600
-		minutes := (data[i] % 3600) / 60
-		center(10, fmt.Sprintf("%02d:%02d", hours, minutes))
+
+		var c string
+		switch i {
+		case VeryProductive:
+			c = color.Blue
+		case Productive:
+			c = color.Cyan
+		case Neutral:
+			c = color.Gray
+		case Distracting:
+			c = color.Yellow
+		case VeryDistracting:
+			c = color.Red
+		}
+		fmt.Print(color.Ize(c, s))
 	}
 	fmt.Println()
 }
@@ -62,6 +90,7 @@ func main() {
 
 	rows := readData(data)
 
+	printHead()
 	for k, i := range rows {
 		printRow(k, i)
 	}
